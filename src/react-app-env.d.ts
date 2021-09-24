5c97110e83dec83721de25df8c8afb13bcac6785
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-24 14:24:23 +0530

    chore(env): added  JEST_WORKER_ID to types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,27 +2,29 @@
 
 // ENV Variables
 declare namespace NodeJS {
-	interface ProcessEnv {
-		NODE_ENV: 'development' | 'production';
-		PUBLIC_URL: string;
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production';
+    PUBLIC_URL: string;
 
-		REACT_APP_SERVER_URL: string;
+    REACT_APP_SERVER_URL: string;
 
-		REACT_APP_FACEBOOK_ID: string;
+    REACT_APP_FACEBOOK_ID: string;
 
-		REACT_APP_GITHUB_CLIENT_ID: string;
-		REACT_APP_GITHUB_CLIENT_SECRET: string;
+    REACT_APP_GITHUB_CLIENT_ID: string;
+    REACT_APP_GITHUB_CLIENT_SECRET: string;
 
-		REACT_APP_GOOGLE_CLIENT_ID: string;
-		REACT_APP_GOOGLE_CLIENT_SECRET: string;
-	}
+    REACT_APP_GOOGLE_CLIENT_ID: string;
+    REACT_APP_GOOGLE_CLIENT_SECRET: string;
+
+    JEST_WORKER_ID: string;
+  }
 }
 
 // Extend the window object
 declare global {
-	interface Window {
-		FB: {
-			getLoginStatus(): void;
-		};
-	}
+  interface Window {
+    FB: {
+      getLoginStatus(): void;
+    };
+  }
 }
