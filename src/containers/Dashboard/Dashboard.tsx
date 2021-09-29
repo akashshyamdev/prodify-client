4e3134f94e796a0bcf42115ef639886f6c3735c5
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-29 10:31:07 +0530

    feat: Dashboard.tsx

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Layout from '../../layout/Layout';
 import Home from './Home/Home';
+import Todos from './Todos/Todos';
 
 export default function Dashboard() {
   return (
     <Layout>
       <Route exact path={'/'} component={Home} />
+
+      <Route exact path={'/todos'} component={Todos} />
     </Layout>
   );
 }
